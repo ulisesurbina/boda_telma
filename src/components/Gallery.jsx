c4207d211749472818bccee89c07f4c140440c8c
Ulises Urbina Maldonado <ulisesurbinam@gmail.com>
2026-03-21 15:56:41 -0600

    UPD grid

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,8 +1,9 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import '../styles/Gallery.css';
 
 import boda1 from '../assets/boda1.jpg';
 import boda2 from '../assets/boda2.jpg';
+import boda3 from '../assets/boda3.jpg';
 import boda4 from '../assets/boda4.jpg';
 import boda6 from '../assets/boda6.jpg';
 import boda7 from '../assets/boda7.jpg';
@@ -23,9 +24,8 @@ import boda21 from '../assets/boda21.jpg';
 import boda22 from '../assets/boda22.png';
 
 const Gallery = () => {
-  // Array de placeholders para las imágenes
   const images = [
-    boda1, boda2, boda4, boda6, boda7, boda8,
+    boda1, boda2, boda3, boda4, boda6, boda7, boda8,
     boda9, boda10, boda11, boda12, boda13, boda14,
     boda15, boda16, boda17, boda18, boda19, boda20,
     boda21, boda22
@@ -47,7 +47,7 @@ const Gallery = () => {
           {images.map((img, index) => (
             <div 
               key={index} 
-              className={`gallery-item item-${index + 1}`}
+              className={`gallery-item item${index + 1}`}
             >
               {!loaded[index] && <div className="gallery-skeleton"></div>}
 
